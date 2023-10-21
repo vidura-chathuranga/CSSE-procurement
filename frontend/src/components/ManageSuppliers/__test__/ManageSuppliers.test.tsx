f34d9b4794762c9e9b92ddab3bb12aab398d34e0
vidura-chahuranga <110117214+vidura-chathuranga@users.noreply.github.com>
2023-10-21 08:34:56 +0530

    add test assertions for supplier frontend

--- frontend/src/components/ManageSuppliers/__test__/ManageSuppliers.test.tsx
+++ frontend/src/components/ManageSuppliers/__test__/ManageSuppliers.test.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
 import ManageSuppliers from "..";
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
 // check if the ManageSuppliers component renders without crashing
-test('renders without crashing - ManageSuppliers', () => {
-    window.ResizeObserver = jest.fn().mockImplementation(() => ({
-        observe: jest.fn(),
-        unobserve: jest.fn(),
-        disconnect: jest.fn(),
-    }));
+test("renders without crashing - ManageSuppliers", () => {
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+  render(<ManageSuppliers />);
+  const linkElement = screen.getByText(/Add Supplier/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+describe("ManageSuppliers Component", () => {
+  it("renders loading message initially", () => {
     render(<ManageSuppliers />);
-    const linkElement = screen.getByText(/Add Supplier/i);
-    expect(linkElement).toBeInTheDocument();
+    const loadingMessage = screen.getByText("Loading");
+    expect(loadingMessage).toBeInTheDocument();
+  });
+
+  it("renders table headers", async () => {
+    render(<ManageSuppliers />);
+    await waitFor(() => {
+      const headers = screen.getAllByRole("columnheader");
+      expect(headers).toHaveLength(6); // Assuming you have 6 columns in your table
+    });
+  });
+
+  it("renders rows with edit and delete buttons", async () => {
+    render(<ManageSuppliers />);
+    await waitFor(() => {
+      const editButtons = screen.getAllByText("Edit");
+      const deleteButtons = screen.getAllByText("Delete");
+      expect(editButtons).toHaveLength(5);
+      expect(deleteButtons).toHaveLength(5);//by assuming table have only 5 records
+    });
+  });
+
+  it("opens edit modal when edit button is clicked", async () => {
+    render(<ManageSuppliers />);
+    await waitFor(() => {
+      const editButton = screen.getByText("Edit");
+      fireEvent.click(editButton);
+      const editModal = screen.getByText("Edit supplier Record");
+      expect(editModal).toBeInTheDocument();
+    });
+  });
+
+
 });
